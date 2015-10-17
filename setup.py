669e0649771a784c6f3c07274775c3670f50b739
Jane Adams <jane@thejunglejane.com>
2015-10-17 15:35:35 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(
     author_email = 'jane@thejunglejane.com',
     license = 'MIT',
     url = 'https://github.com/thejunglejane/pynigma',
-    download_url = 'https://github.com/thejunglejane/pynigma/tarball/0.0.1'
+    download_url = 'https://github.com/thejunglejane/pynigma/tarball/0.0.2'
 )
